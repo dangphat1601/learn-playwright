f6a8c84eb643ff5a47c92202c0ca84bcb7782126
dangphat1601 <danghuuphat1601@gmail.com>
2024-10-21 23:23:47 +0700

    add new lesson

---
+++ pw-course/tests/lesson-6/14-some.js
@@ -0,0 +1,6 @@
+// some() => kiem tra xem co it nhat phan tu trong mang thoa man dieu kien cho truoc
+
+const arr = [1, 2, 3, 4];
+const hasEven = arr.some(num => num % 2 === 0); //true
+
+console.log(hasEven);
---
+++ pw-course/tests/lesson-6/18-sort.js
@@ -0,0 +1,5 @@
+// soft() sap xep cac phan tu cua mang
+
+const arr = [3, 4, 1, 2, 5];
+arr.sort(); // [1, 2, 3, 4]
+console.log(arr);
