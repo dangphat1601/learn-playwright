0e68e47b6dcbb3fdd7bd6cb3f4d5166926f7c76e
dangphat1601 <danghuuphat1601@gmail.com>
2024-10-24 23:22:00 +0700

    continue lesson 06

--- pw-course/tests/lab-6/exercises-01.js
+++ pw-course/tests/lab-6/exercises-01.js
@@ -1,14 +1,14 @@
-// hãy tạo một mảng scores chứa các giá trị điểm của học sinh: 85, 90, 78. Hãy tìm điểm số đầu tiên lớn hơn 80.
-var Excercises01 = /** @class */ (function () {
-    function Excercises01(scores) {
-        this.scores = scores;
-    }
-    Excercises01.prototype.findFirstScore = function () {
-        return this.scores.find(function (score) { return score > 80; });
-    };
-    return Excercises01;
-}());
-var scores = [85, 90, 78];
-var studentScores = new Excercises01(scores);
-var findFirstScore = studentScores.findFirstScore();
-console.log("Diem so dau tien lon hon 80 la: ".concat(findFirstScore));
+// // hãy tạo một mảng scores chứa các giá trị điểm của học sinh: 85, 90, 78. Hãy tìm điểm số đầu tiên lớn hơn 80.
+// var Excercises01 = /** @class */ (function () {
+//     function Excercises01(scores) {
+//         this.scores = scores;
+//     }
+//     Excercises01.prototype.findFirstScore = function () {
+//         return this.scores.find(function (score) { return score > 80; });
+//     };
+//     return Excercises01;
+// }());
+// var scores = [85, 90, 78];
+// var studentScores = new Excercises01(scores);
+// var findFirstScore = studentScores.findFirstScore();
+// console.log("Diem so dau tien lon hon 80 la: ".concat(findFirstScore));
